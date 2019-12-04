d719b54ac9d418710eace57c5f6e77ef7a9ad171
jamshed <mdkhan@cs.stonybrook.edu>
2019-12-04 02:48:00 -0500

    Definition of sortedness and correctness of sorting algorithms copied to BubbleSort; no need to import Insertion sort now.

--- BubbleSort.v
+++ BubbleSort.v
@@ -1,5 +1,18 @@
 From VFA Require Import Perm.
-From VFA Require Import Sort.
+
+
+Inductive sorted: list nat -> Prop := 
+| sorted_nil:
+    sorted nil
+| sorted_1: forall x,
+    sorted (x :: nil)
+| sorted_cons: forall x y l,
+   x <= y -> sorted (y :: l) -> sorted (x :: y :: l).
+
+
+Definition is_a_sorting_algorithm (f: list nat -> list nat) :=
+  forall al, Permutation al (f al) /\ sorted (f al).
+
 
 
 
